8847f3d383e8168f91514370459b1947ec706092
jakefitzm <x18491082@student.ncirl.ie>
2023-07-31 21:00:45 +0100

    Commit Pages

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
-
+#validation before written to table in the database
 class Booking < ApplicationRecord
+    validates :name, :presence => true, length: {minimum:3, message: "must have at least 3 characters"}
 end
